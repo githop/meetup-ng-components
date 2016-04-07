03dfce08983871545a9716eff6312fefc503d968
githop <tom@githop.com>
2016-04-07 11:22:44 -0600

    feat(todos): completed tasks count, clear input after adding new task

--- client/components/appRoot/appRoot.cmp.js
+++ client/components/appRoot/appRoot.cmp.js
@@ -3,23 +3,29 @@
  */
 
 class appRootCtrl {
-    constructor(todosSrv) {
+    constructor($scope, todosSrv) {
         'ngInject';
         this.welcome = 'Everything Seems to be in order this wonderful eve!';
 
         this.$onInit = () => {
             this.todos = todosSrv.fetch;
             this.todoSrv = todosSrv;
+            this.$scope = $scope;
         };
     }
 
     addTodo(todo) {
         this.todoSrv.add(todo);
+        this.$scope.new.todo = '';
     }
 
     remove(todo) {
         this.todoSrv.remove(todo.rm);
     }
+
+    completed() {
+        return this.todos().filter(t => t.done === true).length
+    };
 }
 
 export default {
@@ -27,6 +33,7 @@ export default {
     template: `
     <h3>{{$ctrl.welcome}}</h3>
 
+    {{$ctrl.completed()}} of {{$ctrl.todos().length}} done!
     <input ng-model="new.todo" type="text">
     <button ng-click="$ctrl.addTodo(new.todo)">add</button>
     <gth-todos todos="$ctrl.todos()" on-remove="$ctrl.remove(rm)"></gth-todos>
